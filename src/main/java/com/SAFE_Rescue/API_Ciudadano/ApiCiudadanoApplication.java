5385836387e78151f97c9023bb534e66a44ce1f6
MauriTapia <ma.tapia.ay@gmail.com>
2025-05-30 07:06:54 -0400

    version 3
    Se realiza un modificación en la generación de la API

---
+++ src/main/java/com/SAFE_Rescue/API_Ciudadano/ApiCiudadanoApplication.java
@@ -0,0 +1,13 @@
+package com.SAFE_Rescue.API_Ciudadano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCiudadanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCiudadanoApplication.class, args);
+	}
+
+}
